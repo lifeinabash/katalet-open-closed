0d751aed296b03c0f4a6073bf62c0b338f5d9ded
Harmon Wood (-) <harmon.wood@jwt.com>
2015-06-30 14:31:40 -0500

    I removed all the config code and the name code to simplify to the learned task at hand.

--- src/python/katalet.py
+++ src/python/katalet.py
@@ -1,42 +1,30 @@
 import sys
 
-class Config:
-
-    def __init__(self, cli_args):
-        self.parse_arguments(cli_args)
-
-    def parse_arguments(self, cli_args):
-        try:
-            self.katalet_name = cli_args[0]
-        except:
-            print 'Please enter a katalet name and format [human|json]'
-            exit(2)
-        try:
-            self.out_format = cli_args[1]
-        except:
-            self.out_format = 'human'
-
 class Katalet:
 
-    def __init__(self, katalet_name):
-        self.katalet_name = katalet_name
+    def __init__(self):
 
     def print_name(self, out_format):
         if out_format == 'human':
-            return self.human_format(self.katalet_name)
+            return self.human_format(out_format)
         elif out_format == 'json':
-            return self.json_format(self.katalet_name)
+            return self.json_format(out_format)
 
     def human_format(self, text):
-        return 'The katalet you have selected is %s.' % text
+        return 'The format you have selected is %s.' % text
 
     def json_format(self, text):
-        return '{ name: "%s" }' % text
+        return '{ format: "%s" }' % text
 
 def main(argv):
-    config = Config(argv)
-    katalet = Katalet(config.katalet_name)
-    print katalet.print_name(config.out_format)
+    try:
+        out_format = argv[1]
+    except:
+        print 'Please enter output format [human|json].'
+        exit(2)
+
+    katalet = Katalet()
+    print katalet.print_name(out_format)
 
 if __name__ == "__main__":
         main(sys.argv[1:])
